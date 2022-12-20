22611b517181c16c6c82d89275b02dd31abc1c1d
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-21 01:57:41 +0500

    React Data Fetching

--- client/src/components/JobBoard.js
+++ client/src/components/JobBoard.js
@@ -1,10 +1,15 @@
+import { useEffect, useState } from 'react'
 import JobList from './JobList'
-import { jobs } from '../fake-data'
 import { getJobs } from '../graphql/queries'
 
-getJobs()
-
 function JobBoard() {
+  const [jobs, setJobs] = useState([])
+
+  useEffect(() => {
+    // getJobs().then((jobs) => setJobs(jobs)) <--- same as below
+    getJobs().then(setJobs)
+  }, [])
+
   return (
     <div>
       <h1 className="title">Job Board</h1>
