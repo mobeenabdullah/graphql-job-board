e5c6d22d83589cda3e6dca176960971870032e36
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-12-26 19:51:41 +0500

    Exercise: useCreateJob

--- client/src/graphql/hooks.js
+++ client/src/graphql/hooks.js
@@ -1,5 +1,36 @@
-import { JOBS_QUERY, JOB_QUERY, COMPANY_QUERY } from '../graphql/queries'
-import { useQuery } from '@apollo/client'
+import {
+  JOBS_QUERY,
+  JOB_QUERY,
+  COMPANY_QUERY,
+  CREATE_JOB_MUTATION,
+} from '../graphql/queries'
+import { useMutation, useQuery } from '@apollo/client'
+import { getAccessToken } from '../auth'
+
+export function useCreateJob() {
+  const [mutate, { loading, error }] = useMutation(CREATE_JOB_MUTATION)
+
+  return {
+    createJob: async (title, description) => {
+      const {
+        data: { job },
+      } = await mutate({
+        variables: { input: { title, description } },
+        context: { headers: { Authorization: 'Bearer ' + getAccessToken() } },
+        update: (cache, { data: { job } }) => {
+          cache.writeQuery({
+            query: JOB_QUERY,
+            variables: { id: job.id },
+            data: { job },
+          })
+        },
+      })
+      return job
+    },
+    loading,
+    error: Boolean(error),
+  }
+}
 
 export function useJob(id) {
   const { data, loading, error } = useQuery(JOB_QUERY, {
